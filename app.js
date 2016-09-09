a00d1c5c54d59e9f3491afc8fef9e4bc99691721
Raymond Do <cheapazn@hotmail.com>
2016-09-08 18:33:59 -0700

    Created tile array and new tile objects.

---
+++ app.js
@@ -0,0 +1,10 @@
+tileArr = [];
+
+tileArr.push(new TileObj('red', red));
+tileArr.push(new TileObj('blue', blue));
+tileArr.push(new TileObj('green', green));
+tileArr.push(new TileObj('orange', orange));
+tileArr.push(new TileObj('brown', brown));
+tileArr.push(new TileObj('purple', purple));
+tileArr.push(new TileObj('yellow', yellow));
+tileArr.push(new TileObj('cyan', cyan));
