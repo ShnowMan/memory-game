92cc0d398b1ceebeecbb022846ffa6218a0d5a21
Raymond Do <cheapazn@hotmail.com>
2016-09-08 21:39:09 -0700

    Made random tile index logic generator.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var tileArr = [];
+var tileObjArr = [];
+var tempInd = [];
+var randomTileInd = [];
 
 function TileObj(color) {
   this.name = color;
-  tileArr.push(this);
+  tileObjArr.push(this);
 };
 
 var createTiles = function() {
@@ -15,3 +17,29 @@ var createTiles = function() {
   new TileObj('yellow');
   new TileObj('cyan');
 };
+
+var tracker = {
+  randomInd: [],
+  makeTileInd: function() {
+    for (var i = 1; i <= 16; i++) {
+      tempInd.push(i);
+    }
+  },
+  randomizeTileIndex: function() {
+    while (tempInd.length > 0) {
+    // for (var i = 0; i < 16; i++) {
+      var idx = Math.floor(Math.random() * tempInd.length);
+      var thing = tempInd[idx];
+      tempInd.splice(idx, 1);
+      randomTileInd.push(thing);
+    }
+  },
+
+};
+
+createTiles();
+createTiles();
+tracker.makeTileInd();
+tracker.randomizeTileIndex();
+console.log(randomTileInd);
+// console.table(randomTileInd);
