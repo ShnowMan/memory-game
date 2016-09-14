ff55d6f0ce545a76af677daa51469d2c9ff84d45
Raymond Do <cheapazn@hotmail.com>
2016-09-13 19:30:16 -0700

    Added sound effects.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var tileObjArr = [];
 var tempIdxArr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 var randomTileIdxArr = [];
+var wrongSound = new Audio('http://www.wavsource.com/snds_2016-08-21_1204101428963685/tv/simpsons/homer/aw_crap.wav');
+var correctSound = new Audio('http://noproblo.dayjo.org/ZeldaSounds/LOZ/LOZ_Fanfare.wav');
+var finishSound = new Audio('http://themushroomkingdom.net/sounds/wav/smb/smb_stage_clear.wav');
 
 function TileObj(color) {
   this.color = color;
@@ -46,7 +49,15 @@ var tracker = {
     tracker.tileFlipCount++;
     tracker.tileElArr[tileNum].removeEventListener('click', tracker.flip);
     if (tracker.tileFlipCount >= 2) {
+      tracker.playSound();
       setTimeout(tracker.compareTiles,500);
+    }
+  },
+  playSound: function() {
+    if (tracker.tileColorCompareArr[0] === tracker.tileColorCompareArr[1]) {
+      correctSound.play();
+    } else {
+      wrongSound.play();
     }
   },
   compareTiles: function() {
@@ -54,6 +65,7 @@ var tracker = {
     if (tracker.tileColorCompareArr[0] === tracker.tileColorCompareArr[1]) {
       tracker.tileColorCompareArr = [];
       if (tracker.tileMatchArr.length >= tileObjArr.length) {
+        finishSound.play();
         alert('Congrats!');
       }
     } else {
